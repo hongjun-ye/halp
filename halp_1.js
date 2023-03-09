77c17c37579b64a1dd932c07a87676c7c4556ee7
Hongjun Ye <46825173+hongjun-ye@users.noreply.github.com>
2023-03-09 11:06:58 -0500

    Update halp_1.js

--- halp_1.js
+++ halp_1.js
@@ -29,8 +29,8 @@ localStorage.setItem("stocks", stocksJson);
 function updateCashOnHand_A() {
   let totalBuyCost = 0;
   let totalSellRevenue = 0;
-  totalBuyCost += stocks["A"].change * stockPriceA;
-  totalSellRevenue += stocks["A"].change * stockPriceA;
+  totalBuyCost += stocks["A"].change * stockPrices["A"];
+  totalSellRevenue += stocks["A"].change * stockPrices["A"];
   cashOnHand += totalSellRevenue - totalBuyCost;
   document.getElementById("cash-on-hand").innerText = cashOnHand.toLocaleString(
     "en-US",
